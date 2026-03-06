6bd5f2f10b54483a988a2f15e92a085ff6ea0db2
nurhaliza meilinda iswanto <112323838+lizaa4@users.noreply.github.com>
2026-03-06 19:52:09 +0700

    Update streamlit_app.py

--- dashboard/streamlit_app.py
+++ dashboard/streamlit_app.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 st.title("Smart Farming Dashboard")
 
-df = pd.read_csv("../outputs/cleaned_data.csv")
+df = pd.read_csv("outputs/cleaned_data.csv")
 
 st.write("Preview Data")
 st.dataframe(df)
