cc08ebb21cc3d1bdc5a9c226f68e2f2d67bf2d10
nurhaliza meilinda iswanto <112323838+lizaa4@users.noreply.github.com>
2026-03-06 19:44:20 +0700

    Update streamlit_app.py

--- dashboard/streamlit_app.py
+++ dashboard/streamlit_app.py
@@ -1,26 +1,9 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
 
 st.title("Smart Farming Dashboard")
 
-# load data
-df = pd.read_csv("smart_farming_cleaned.csv")
+df = pd.read_csv("../outputs/cleaned_data.csv")
 
-# tampilkan data
-st.subheader("Dataset Preview")
-st.write(df.head())
-
-# grafik temperature
-st.subheader("Temperature Distribution")
-
-fig, ax = plt.subplots()
-ax.hist(df["temperature"])
-st.pyplot(fig)
-
-# grafik soil moisture
-st.subheader("Soil Moisture Distribution")
-
-fig2, ax2 = plt.subplots()
-ax2.hist(df["soil_moisture"])
-st.pyplot(fig2)
+st.write("Preview Data")
+st.dataframe(df)
