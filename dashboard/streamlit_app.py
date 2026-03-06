6baeab4da063ad5a5f893c6e20183d2106eb95ee
nurhaliza meilinda iswanto <112323838+lizaa4@users.noreply.github.com>
2026-03-06 19:55:33 +0700

    Update streamlit_app.py

--- dashboard/streamlit_app.py
+++ dashboard/streamlit_app.py
@@ -5,5 +5,14 @@ st.title("Smart Farming Dashboard")
 
 df = pd.read_csv("outputs/cleaned_data.csv")
 
-st.write("Preview Data")
+st.subheader("Preview Data")
 st.dataframe(df)
+
+st.subheader("Jumlah Jenis Tanaman")
+st.bar_chart(df["crop_type"].value_counts())
+
+st.subheader("Rata-rata Soil Moisture per Region")
+st.bar_chart(df.groupby("region")["soil_moisture_%"].mean())
+
+st.subheader("Distribusi Temperature")
+st.line_chart(df["temperature_C"])
